d34e008d469fb3856fb2219800d435128a4107d6
TrungKien1230 <trungkiennguyen28995@gmail.com>
2023-01-02 16:11:30 +0100

    home page

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -25,6 +25,19 @@ SECRET_KEY = 'eqgugeh$*8p^m3o&9_-sx42huebf_5-(vjmr6324rt7ihi-1hh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = "username"
+    EMAIL_HOST = 'smtp.domain.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = "password"
+else:
+    EMAIL_BACKEND = (
+
+        "django.core.mail.backends.console.EmailBackend"
+    )
+
 ALLOWED_HOSTS = []
 
 
@@ -37,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
